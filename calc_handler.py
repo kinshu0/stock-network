9cfc4e25b4b86ed8d89dc22ea54363bc74149823
Kinshu Gupta <kinshugupta2002@gmail.com>
2020-06-28 18:27:37 -0400

    insert method for calculated matrix mesh

--- calc_handler.py
+++ calc_handler.py
@@ -7,8 +7,6 @@ server = Default_Server()
 
 ticker_list = server.stock_collection.tickers_list()
 
-start = "20071201"
-end = "20090630"
 
 def feed_to_dict(booolla):
     haha = dict()
@@ -45,71 +43,77 @@ def fetch_multiple_matrix_ts(tickers, start=None, end=None):
     b = list(zip(tickers, a))
     return b
 
-def calculate_insert_mesh(list_of_tickers, start, end):
-    ticker_combinations = list(combinations(list_of_tickers, 2))
+# def calculate_insert_mesh(list_of_tickers, start, end):
+#     ticker_combinations = list(combinations(list_of_tickers, 2))
     
-    c = 0
-    n = len(ticker_combinations)
+#     c = 0
+#     n = len(ticker_combinations)
 
-    result = []
-    '''
-    This part is for processing the data so it's a single matrix formatted as
-    columns: open high low close volume
-    rows: each corresponding time
-    '''
-    for i in ticker_combinations:
-        first = server.stock_collection.retrieve_ts(i[0], start, end)
-        second = server.stock_collection.retrieve_ts(i[1], start, end)
+#     result = []
+#     '''
+#     This part is for processing the data so it's a single matrix formatted as
+#     columns: open high low close volume
+#     rows: each corresponding time
+#     '''
+#     for i in ticker_combinations:
+#         first = server.stock_collection.retrieve_ts(i[0], start, end)
+#         second = server.stock_collection.retrieve_ts(i[1], start, end)
 
-        first_dict = feed_to_dict(first)
-        second_dict = feed_to_dict(second)
+#         first_dict = feed_to_dict(first)
+#         second_dict = feed_to_dict(second)
 
 
-        if len(first_dict) == 0 or len(second_dict) == 0:
-            continue
-        if not len(first_dict) == len(second_dict):
-            # raise Exception("Yeaaaa...Nahhh. They don't correspond bro")
-            continue
+#         if len(first_dict) == 0 or len(second_dict) == 0:
+#             continue
+#         if not len(first_dict) == len(second_dict):
+#             # raise Exception("Yeaaaa...Nahhh. They don't correspond bro")
+#             continue
         
-        keys = first_dict.keys()
+#         keys = first_dict.keys()
 
-        vals_1 = []
-        vals_2 = []
+#         vals_1 = []
+#         vals_2 = []
 
-        for x in keys:
-            try:
-                vals_2.append(second_dict[x])
-                vals_1.append(first_dict[x])
-            except KeyError:
-                continue
+#         for x in keys:
+#             try:
+#                 vals_2.append(second_dict[x])
+#                 vals_1.append(first_dict[x])
+#             except KeyError:
+#                 continue
 
-        '''
-        For now I'm only performing analysis on the closing price,
-        add more later
-        '''
-        close_prices_1 = [x[3] for x in vals_1]
-        close_prices_2 = [x[3] for x in vals_2]
+#         '''
+#         For now I'm only performing analysis on the closing price,
+#         add more later
+#         '''
+#         close_prices_1 = [x[3] for x in vals_1]
+#         close_prices_2 = [x[3] for x in vals_2]
 
-        r = do(close_prices_1, close_prices_2)
+#         r = do(close_prices_1, close_prices_2)
 
-        post = {
-            'xy': i,
-            'r': r
-        }
-        result.append(post)
-    return result
+#         post = {
+#             'xy': i,
+#             'r': r
+#         }
+#         result.append(post)
+#     return result
 
-        # server.association_collection.insert(post)
+#         # server.association_collection.insert(post)
 
-        # c += 1
-        # print(f'{c}/{n} {i} Done')
+#         # c += 1
+#         # print(f'{c}/{n} {i} Done')
 
 # calculate_insert_mesh(ticker_list, start, end)
 
 def insert_mesh(calculated_mesh):
-    for post in calculated_mesh:
+    for x in calculated_mesh:
+        post = {
+            'xy': [x[0], x[1]],
+            'r': x[2]
+        }
         server.association_collection.insert(post)
 
+start = "20071201"
+end = "20090630"
 z = fetch_multiple_matrix_ts(ticker_list, start, end)
 k = calc_mesh(z)
 print(k)
