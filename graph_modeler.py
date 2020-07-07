47292787cafdfc18c8a5ba674c1a2e9f7fe17b93
Kinshu Gupta <kinshugupta2002@gmail.com>
2020-07-07 17:38:15 -0400

    Working on admin tools next

--- graph_modeler.py
+++ graph_modeler.py
@@ -1,14 +1,244 @@
+
 import networkx as nx
-import matplotlib.pyplot as plt
+import plotly.graph_objs as go
 
-G = nx.Graph()
+import numpy as np
+import pandas as pd
+from colour import Color
 
-def add_stonks(list_of_tickers, correlation_list, threshold = 0.6):
-    G.add_nodes_from(list_of_tickers)
-    for y in correlation_list:
-        if y[2][4] >= threshold:
-            post = (y[0], y[1])
-            G.add_edge(*post)
+import client
 
-def export_graph():
-    nx.write_gexf(G, 'works.gexf')
+
+stock_attributes = client.retrieve_ticker_attributes()
+stock_atts = pd.DataFrame(stock_attributes, columns=['Ticker', 'Sector', 'Company_Name'])
+
+palette = [
+    'rgb(254,255,88)',
+    'rgb(255,170,40)',
+    'rgb(255,25,95)',
+    'rgb(176,77,221)',
+    'rgb(80,233,255)',
+    'rgb(126,234,188)',
+    'rgb(255,201,216)',
+    'rgb(0.9,0.859,0.039)',
+    'rgb(0.918,0.988,0.039)',
+    'rgb(0.9,0.714,0.039)',
+    'rgb(0.565,0.925,0.035)'
+]
+
+def network_graph(yearRange, ticker_to_search, threshold=0.6, mode='Sector'):
+
+    # start = int(f'{yearRange[0]}0101')
+    # end = int(f'{yearRange[1]}1231')
+    start = yearRange[0]
+    end = yearRange[1]
+
+    mesh = client.retrieve_mesh(start, end)
+
+    stock_returns = client.retrieve_returns(start, end)
+    return_atts = pd.DataFrame(stock_returns, columns=['Ticker', 'open', 'high', 'low', 'close', 'volume'])
+
+    edges_df = pd.DataFrame(mesh, columns=['Symbol1', 'Symbol2', 'r_open', 'r_high', 'r_low', 'r_close', 'r_volume'])
+    
+    #drop edges with correlation less than threshold
+    indices = []
+
+    for index in range(0,len(edges_df)):
+        if edges_df['r_close'][index] < threshold:
+            indices.append(index)
+            # edges_df.drop(axis=0, index=index, inplace=True)
+            # continue
+    edges_df.drop(indices, inplace=True)
+
+
+    node_atts = return_atts
+
+    node_atts['Sector'] = stock_atts['Sector']
+    node_atts['Company_Name'] = stock_atts['Company_Name']
+
+    
+    if not mode == 'close':
+        uniques = set(node_atts[mode])
+        color_key = {}
+        i = 0
+        for x in uniques:
+            color_key.update({
+                x: palette[i]
+            })
+            i+=1
+    elif mode == 'close':
+        uniques = set(node_atts[mode])
+
+        l = 0.6
+
+        neutral = Color('gray', luminance = l)
+        positive = Color('green', luminance = l)
+        negative = Color('red', luminance = l)
+        color_step = 10
+        color_range = [*list(negative.range_to(neutral, int(color_step/2))), *list(neutral.range_to(positive, int(color_step/2)))]
+
+        # val_middle = 1
+        # val_end = max( abs(max(uniques)-val_middle), abs(min(uniques) - val_middle) )
+
+        # a = np.linspace(-val_end, val_end, color_step)
+
+        a = np.linspace(-2, 2, color_step)
+
+        # b = np.linspace(-val_end, val_middle, int(color_step/2))
+        # c = np.linspace(val_middle, val_end, int(color_step/2))
+        # a = [*b, *c]
+
+        color_key = {}
+
+        for k in uniques:
+            color_key.update({
+                k: min(a, key=lambda x:abs(x-k))
+            })
+        for z in color_key:
+            # color_key[z] = f'rgb{color_range[int(  min(a, key=lambda x:abs(x-z)))].rgb}'
+            # color_key[z] = f'rgb{color_range[  min(a, key=lambda x:abs(x-z)))].rgb}'
+            color_key[z] = f'rgb{color_range[  list(a).index( color_key[z] )  ].rgb}'
+            # color_key[z] = f'rgb{Color("orange").rgb}'
+        
+        
+
+
+
+
+    ticker_set = set(client.ticker_list)
+
+
+    
+    G = nx.from_pandas_edgelist(edges_df, 'Symbol1', 'Symbol2', edge_attr=True)
+
+    for col in node_atts.columns:
+        if col == 'Ticker':
+            continue
+        nx.set_node_attributes(G, node_atts.set_index('Ticker')[col].to_dict(), col)
+
+    # if ticker_to_search in G.nodes:
+    #     pos = nx.layout.spring_layout(G, center=ticker_to_search)
+    # else:
+    #     pos = nx.layout.spring_layout(G)
+
+    
+    pos = nx.layout.spring_layout(G)
+
+    for node in G.nodes:
+        G.nodes[node]['pos'] = list(pos[node])
+
+
+
+    traceRecode = []  # contains edge_trace, node_trace, middle_node_trace
+    ############################################################################################################################################################
+    colors = list(Color('lightcoral').range_to(Color('darkred'), len(G.edges())))
+    colors = ['rgb' + str(x.rgb) for x in colors]
+
+
+
+    index = 0
+    for edge in G.edges:
+
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+
+
+        weight = float(G.edges[edge]['r_close']) / max(edges_df['r_close']) * 10
+
+        trace = go.Scatter(x=tuple([x0, x1, None]), y=tuple([y0, y1, None]),
+                           mode='lines',
+                           line={'width': weight},
+                           marker=dict(color=colors[index]),
+                           line_shape='spline',
+                           opacity=1)
+        traceRecode.append(trace)
+        index = index + 1
+    ###############################################################################################################################################################
+    node_trace = go.Scatter(x=[], y=[], hovertext=[], text=[], mode='markers+text', textposition="middle center",
+                            hoverinfo="text", marker={'size': 50, 'color': []})
+
+    index = 0
+    for node in G.nodes():
+        x, y = G.nodes[node]['pos']
+        each_stock_return = '{:.2f}'.format((G.nodes[node]["close"] - 1) * 100)
+        hovertext = (
+            f'Company Name: {G.nodes[node]["Company_Name"]}<br>'
+            f'Sector: {G.nodes[node]["Sector"]}<br>'
+            f'Return: {each_stock_return}%'
+        )
+
+        text = node
+
+        node_trace['marker']['color'] += tuple( [color_key[G.nodes[node][mode]]] )
+
+        node_trace['x'] += tuple([x])
+        node_trace['y'] += tuple([y])
+        node_trace['hovertext'] += tuple([hovertext])
+        node_trace['text'] += tuple([text])
+        index = index + 1
+
+    traceRecode.append(node_trace)
+    ################################################################################################################################################################
+    middle_hover_trace = go.Scatter(x=[], y=[], hovertext=[], mode='markers', hoverinfo="text",
+                                    marker={'size': 20, 'color': 'LightSkyBlue'},
+                                    opacity=0)
+
+    index = 0
+    for edge in G.edges:
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+        
+        # hovertext = f'close r: {str(G.edges[edge]["r_close"])}<br>volume r:{str(G.edges[edge]["r_volume"])}'
+        hovertext = (
+            f'r_coefficient: {G.edges[edge]["r_close"]}'
+        )
+
+        middle_hover_trace['x'] += tuple([(x0 + x1) / 2])
+        middle_hover_trace['y'] += tuple([(y0 + y1) / 2])
+        middle_hover_trace['hovertext'] += tuple([hovertext])
+        index = index + 1
+
+    traceRecode.append(middle_hover_trace)
+    #################################################################################################################################################################
+    figure = {
+        "data": traceRecode,
+        "layout": go.Layout(title='Stock Network Correlation', showlegend=False, hovermode='closest',
+                            margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
+                            xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                            yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                            height=600,
+                            # annotations=[
+                            #     dict(
+                            #         ax=(G.nodes[edge[0]]['pos'][0] + G.nodes[edge[1]]['pos'][0]) / 2,
+                            #         ay=(G.nodes[edge[0]]['pos'][1] + G.nodes[edge[1]]['pos'][1]) / 2, axref='x', ayref='y',
+                            #         x=(G.nodes[edge[1]]['pos'][0] * 3 + G.nodes[edge[0]]['pos'][0]) / 4,
+                            #         y=(G.nodes[edge[1]]['pos'][1] * 3 + G.nodes[edge[0]]['pos'][1]) / 4, xref='x', yref='y',
+                            #         showarrow=False,
+                            #         # arrowhead=3,
+                            #         # arrowsize=4,
+                            #         # arrowwidth=1,
+                            #         opacity=1
+                            #     ) for edge in G.edges]
+                            )}
+    # figure = {
+    #     "data": traceRecode,
+    #     "layout": go.Layout(title='Interactive Transaction Visualization', showlegend=False, hovermode='closest',
+    #                         margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
+    #                         xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+    #                         yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+    #                         height=600,
+    #                         clickmode='event+ssymbolct',
+    #                         annotations=[
+    #                             dict(
+    #                                 ax=(G.nodes[edge[0]]['pos'][0] + G.nodes[edge[1]]['pos'][0]) / 2,
+    #                                 ay=(G.nodes[edge[0]]['pos'][1] + G.nodes[edge[1]]['pos'][1]) / 2, axref='x', ayref='y',
+    #                                 x=(G.nodes[edge[1]]['pos'][0] * 3 + G.nodes[edge[0]]['pos'][0]) / 4,
+    #                                 y=(G.nodes[edge[1]]['pos'][1] * 3 + G.nodes[edge[0]]['pos'][1]) / 4, xref='x', yref='y',
+    #                                 showarrow=True,
+    #                                 arrowhead=3,
+    #                                 arrowsize=4,
+    #                                 arrowwidth=1,
+    #                                 opacity=1
+    #                             ) for edge in G.edges]
+    #                         )}
+    return figure
--- webapp.py
+++ webapp.py
@@ -1,213 +1,25 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import networkx as nx
-import plotly.graph_objs as go
 
 import pandas as pd
-from colour import Color
-from datetime import datetime
 from textwrap import dedent as d
 import json
 
+from graph_modeler import network_graph
 import client
 
+from datetime import datetime as dt
+
 # import the css template, and pass the css template into dash
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)#, external_stylesheets=external_stylesheets)
 app.title = "Stock Correlation Network"
 
 YEAR=[2010, 2019]
-ACCOUNT="A0001"
-
-stock_attributes = client.retrieve_ticker_attributes()
-stock_atts = pd.DataFrame(stock_attributes, columns=['Ticker', 'Sector', 'Company_Name'])
-
-##############################################################################################################################################################
-def network_graph(yearRange, ticker_to_search, threshold=0.6):
-
-    start = int(f'{yearRange[0]}0101')
-    end = int(f'{yearRange[1]}1231')
-
-    mesh = client.retrieve_mesh(start, end)
-
-    stock_returns = client.retrieve_returns(start, end)
-    return_atts = pd.DataFrame(stock_returns, columns=['Ticker', 'open', 'high', 'low', 'close', 'volume'])
-
-    edges_df = pd.DataFrame(mesh, columns=['Symbol1', 'Symbol2', 'r_open', 'r_high', 'r_low', 'r_close', 'r_volume'])
-    
-    for index in range(0,len(edges_df)):
-        if edges_df['r_close'][index] < threshold:
-            edges_df.drop(axis=0, index=index, inplace=True)
-            continue
+search="BAC"
 
 
-    node_atts = return_atts
-
-    node_atts['Sector'] = stock_atts['Sector']
-    node_atts['Company_Name'] = stock_atts['Company_Name']
-
-    ticker_set = set(client.ticker_list)
-
-
-    # to define the centric point of the networkx layout
-    shells=[]
-    shell1=[]
-    shell1.append(ticker_to_search)
-    shells.append(shell1)
-    shell2=[]
-    for symbol in ticker_set:
-        if symbol!=ticker_to_search:
-            shell2.append(symbol)
-    shells.append(shell2)
-
-    G = nx.from_pandas_edgelist(edges_df, 'Symbol1', 'Symbol2', edge_attr=True)
-
-    for col in node_atts.columns:
-        if col == 'Ticker':
-            continue
-        nx.set_node_attributes(G, node_atts.set_index('Ticker')[col].to_dict(), col)
-
-
-    # # pos = nx.layout.spring_layout(G)
-    # # pos = nx.layout.circular_layout(G)
-    # # nx.layout.shell_layout only works for more than 3 nodes
-    if len(shell2)>1:
-        pos = nx.drawing.layout.shell_layout(G, shells)
-    else:
-        pos = nx.drawing.layout.spring_layout(G)
-    for node in G.nodes:
-        G.nodes[node]['pos'] = list(pos[node])
-
-
-    if len(shell2)==0:
-        traceRecode = []  # contains edge_trace, node_trace, middle_node_trace
-
-        node_trace = go.Scatter(x=tuple([1]), y=tuple([1]), text=tuple([str(ticker_to_search)]), textposition="bottom center",
-                                mode='markers+text',
-                                marker={'size': 50, 'color': 'LightSkyBlue'})
-        traceRecode.append(node_trace)
-
-        node_trace1 = go.Scatter(x=tuple([1]), y=tuple([1]),
-                                mode='markers',
-                                marker={'size': 50, 'color': 'LightSkyBlue'},
-                                opacity=0)
-        traceRecode.append(node_trace1)
-
-        figure = {
-            "data": traceRecode,
-            "layout": go.Layout(title='Interactive Transaction Visualization', showlegend=False,
-                                margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
-                                xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-                                yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-                                height=600
-                                )}
-        return figure
-
-
-    traceRecode = []  # contains edge_trace, node_trace, middle_node_trace
-    ############################################################################################################################################################
-    colors = list(Color('lightcoral').range_to(Color('darkred'), len(G.edges())))
-    colors = ['rgb' + str(x.rgb) for x in colors]
-
-
-
-    index = 0
-    for edge in G.edges:
-
-        x0, y0 = G.nodes[edge[0]]['pos']
-        x1, y1 = G.nodes[edge[1]]['pos']
-
-
-        weight = float(G.edges[edge]['r_close']) / max(edges_df['r_close']) * 10
-        trace = go.Scatter(x=tuple([x0, x1, None]), y=tuple([y0, y1, None]),
-                           mode='lines',
-                           line={'width': weight},
-                           marker=dict(color=colors[index]),
-                           line_shape='spline',
-                           opacity=1)
-        traceRecode.append(trace)
-        index = index + 1
-    ###############################################################################################################################################################
-    node_trace = go.Scatter(x=[], y=[], hovertext=[], text=[], mode='markers+text', textposition="bottom center",
-                            hoverinfo="text", marker={'size': 50, 'color': 'LightSkyBlue'})
-
-    index = 0
-    for node in G.nodes():
-        x, y = G.nodes[node]['pos']
-        hovertext = "Company Name: " + str(G.nodes[node]['Company_Name']) + "<br>" + "Sector: " + str(
-            G.nodes[node]['Sector'])
-
-        text = stock_atts['Ticker'][index]
-        node_trace['x'] += tuple([x])
-        node_trace['y'] += tuple([y])
-        node_trace['hovertext'] += tuple([hovertext])
-        node_trace['text'] += tuple([text])
-        index = index + 1
-
-    traceRecode.append(node_trace)
-    ################################################################################################################################################################
-    middle_hover_trace = go.Scatter(x=[], y=[], hovertext=[], mode='markers', hoverinfo="text",
-                                    marker={'size': 20, 'color': 'LightSkyBlue'},
-                                    opacity=0)
-
-    index = 0
-    # for edge in G.edges:
-    #     x0, y0 = G.nodes[edge[0]]['pos']
-    #     x1, y1 = G.nodes[edge[1]]['pos']
-    #     hovertext = "From: " + str(G.edges[edge]['Source']) + "<br>" + "To: " + str(
-    #         G.edges[edge]['Target']) + "<br>" + "TransactionAmt: " + str(
-    #         G.edges[edge]['TransactionAmt']) + "<br>" + "TransactionDate: " + str(G.edges[edge]['Date'])
-    #     middle_hover_trace['x'] += tuple([(x0 + x1) / 2])
-    #     middle_hover_trace['y'] += tuple([(y0 + y1) / 2])
-    #     middle_hover_trace['hovertext'] += tuple([hovertext])
-    #     index = index + 1
-
-    traceRecode.append(middle_hover_trace)
-    #################################################################################################################################################################
-    figure = {
-        "data": traceRecode,
-        "layout": go.Layout(title='Interactive Transaction Visualization', showlegend=False, hovermode='closest',
-                            margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
-                            xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-                            yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-                            height=600,
-                            annotations=[
-                                dict(
-                                    ax=(G.nodes[edge[0]]['pos'][0] + G.nodes[edge[1]]['pos'][0]) / 2,
-                                    ay=(G.nodes[edge[0]]['pos'][1] + G.nodes[edge[1]]['pos'][1]) / 2, axref='x', ayref='y',
-                                    x=(G.nodes[edge[1]]['pos'][0] * 3 + G.nodes[edge[0]]['pos'][0]) / 4,
-                                    y=(G.nodes[edge[1]]['pos'][1] * 3 + G.nodes[edge[0]]['pos'][1]) / 4, xref='x', yref='y',
-                                    showarrow=True,
-                                    arrowhead=3,
-                                    arrowsize=4,
-                                    arrowwidth=1,
-                                    opacity=1
-                                ) for edge in G.edges]
-                            )}
-    # figure = {
-    #     "data": traceRecode,
-    #     "layout": go.Layout(title='Interactive Transaction Visualization', showlegend=False, hovermode='closest',
-    #                         margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
-    #                         xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-    #                         yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-    #                         height=600,
-    #                         clickmode='event+ssymbolct',
-    #                         annotations=[
-    #                             dict(
-    #                                 ax=(G.nodes[edge[0]]['pos'][0] + G.nodes[edge[1]]['pos'][0]) / 2,
-    #                                 ay=(G.nodes[edge[0]]['pos'][1] + G.nodes[edge[1]]['pos'][1]) / 2, axref='x', ayref='y',
-    #                                 x=(G.nodes[edge[1]]['pos'][0] * 3 + G.nodes[edge[0]]['pos'][0]) / 4,
-    #                                 y=(G.nodes[edge[1]]['pos'][1] * 3 + G.nodes[edge[0]]['pos'][1]) / 4, xref='x', yref='y',
-    #                                 showarrow=True,
-    #                                 arrowhead=3,
-    #                                 arrowsize=4,
-    #                                 arrowwidth=1,
-    #                                 opacity=1
-    #                             ) for edge in G.edges]
-    #                         )}
-    return figure
-######################################################################################################################################################################
 # styles: for right side hover/click component
 styles = {
     'pre': {
@@ -229,6 +41,11 @@ app.layout = html.Div([
             html.Div(
                 className="two columns",
                 children=[
+                    # dcc.Markdown(d("""
+                    #         **Time Range To Visualize**
+
+                    #         Slide the bar to define year range.
+                    #         """)),
                     dcc.Markdown(d("""
                             **Time Range To Visualize**
 
@@ -256,6 +73,11 @@ app.layout = html.Div([
                                     2019: {'label': '2019'}
                                 }
                             ),
+                            # dcc.DatePickerRange(
+                            #     id='date-picker-range',
+                            #     start_date=dt(2005, 1, 1),
+                            #     end_date=dt.today()
+                            # ),
                             html.Br(),
                             html.Div(id='output-container-range-slider')
                         ],
@@ -281,38 +103,38 @@ app.layout = html.Div([
             html.Div(
                 className="eight columns",
                 children=[dcc.Graph(id="my-graph",
-                                    figure=network_graph(YEAR, ACCOUNT))],
+                                    figure=network_graph(YEAR, search))],
             ),
 
             #########################################right side two output component
-            html.Div(
-                className="two columns",
-                children=[
-                    html.Div(
-                        className='twelve columns',
-                        children=[
-                            dcc.Markdown(d("""
-                            **Hover Data**
+            # html.Div(
+            #     className="two columns",
+            #     children=[
+            #         html.Div(
+            #             className='twelve columns',
+            #             children=[
+            #                 dcc.Markdown(d("""
+            #                 **Hover Data**
 
-                            Mouse over values in the graph.
-                            """)),
-                            html.Pre(id='hover-data', style=styles['pre'])
-                        ],
-                        style={'height': '400px'}),
+            #                 Mouse over values in the graph.
+            #                 """)),
+            #                 html.Pre(id='hover-data', style=styles['pre'])
+            #             ],
+            #             style={'height': '400px'}),
 
-                    html.Div(
-                        className='twelve columns',
-                        children=[
-                            dcc.Markdown(d("""
-                            **Click Data**
+            #         html.Div(
+            #             className='twelve columns',
+            #             children=[
+            #                 dcc.Markdown(d("""
+            #                 **Click Data**
 
-                            Click on points in the graph.
-                            """)),
-                            html.Pre(id='click-data', style=styles['pre'])
-                        ],
-                        style={'height': '400px'})
-                ]
-            )
+            #                 Click on points in the graph.
+            #                 """)),
+            #                 html.Pre(id='click-data', style=styles['pre'])
+            #             ],
+            #             style={'height': '400px'})
+            #     ]
+            # )
         ]
     )
 ])
@@ -321,22 +143,37 @@ app.layout = html.Div([
 @app.callback(
     dash.dependencies.Output('my-graph', 'figure'),
     [dash.dependencies.Input('my-range-slider', 'value'), dash.dependencies.Input('input1', 'value')])
-def update_output(value,input1):
+    # dash.dependencies.Output('my-graph', 'figure'),
+    # [
+    #     # dash.dependencies.Input('my-range-slider', 'value'),
+    #     dash.dependencies.Input('input1', 'value'),
+    #     dash.dependencies.Input('date-picker-range', 'start_date'),
+    #     dash.dependencies.Input('date-picker-range', 'end_date')
+    # ])
+def update_output(value, input1):
+# def update_output(input1, start, end):
     YEAR = value
-    ACCOUNT = input1
+    # value = [start[:10].replace('-', ''), end[:10].replace('-', '')]
+
+    search = input1
     return network_graph(value, input1)
-    # to update the global variable of YEAR and ACCOUNT
+    # to update the global variable of YEAR and search
 ################################callback for right side components
-@app.callback(
-    dash.dependencies.Output('hover-data', 'children'),
-    [dash.dependencies.Input('my-graph', 'hoverData')])
+
+
+
+# @app.callback(
+#     dash.dependencies.Output('hover-data', 'children'),
+#     [dash.dependencies.Input('my-graph', 'hoverData')])
+
 def display_hover_data(hoverData):
     return json.dumps(hoverData, indent=2)
 
 
-@app.callback(
-    dash.dependencies.Output('click-data', 'children'),
-    [dash.dependencies.Input('my-graph', 'clickData')])
+# @app.callback(
+#     dash.dependencies.Output('click-data', 'children'),
+#     [dash.dependencies.Input('my-graph', 'clickData')])
+
 def display_click_data(clickData):
     return json.dumps(clickData, indent=2)
 
