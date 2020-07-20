78a18ca4e025cb45caf95f1e31c8782f6f348353
Tanu Gupta <kinshugupta2002@gmail.com>
2020-07-20 19:17:38 -0400

    Fixed minor error and implemented multiprocessing

--- admin.py
+++ admin.py
@@ -1,14 +1,16 @@
-import core_modules.feed_data as feed_data
+# import core_modules.feed_data as feed_data
 
-API_key = 'HB8BGAK56N2T7BBE'
-file_name = 'symbols.csv'
-# n = 25 
-sleep_period = 13
+# API_key = 'HB8BGAK56N2T7BBE'
+# file_name = 'symbols.csv'
+# # n = 25 
+# sleep_period = 13
 
-feed_data.feed_to_db(file_name, API_key)
+# feed_data.feed_to_db(file_name, API_key)
 
 import core_modules.client as client
 from itertools import combinations
+
+import concurrent.futures
 
 print('Data Fetched, Next: Calculations')
 dates = list(range(2010, 2020))
@@ -16,7 +18,7 @@ date_combinations = combinations(dates, 2)
 date_ranges = []
 
 for x in date_combinations:
-    if x[1] > x[0]:
+    if x[1] < x[0]:
         date_ranges.append( (x[1], x[0]) )
     else:
         date_ranges.append( x )
@@ -26,7 +28,18 @@ for x in date_ranges:
     date_ranges_final.append( (f'{x[0]}0101', f'{x[1]}1231') )
 
 c = 1
-for x in date_ranges_final:
-    client.retrieve_mesh(x[0], x[1])
-    print(f'{x[0]} -> {x[1]} {c}/{len(date_ranges_final)} done')
+def multi_calc(abc):
+    global c
+    client.initialize_dates_data(abc[0], abc[1])
+    print(f'{abc[0]} -> {abc[1]} {c}/{len(date_ranges_final)} done')
     c += 1
+
+if __name__ == '__main__':
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(multi_calc, date_ranges_final)
+
+# c = 1
+# for x in date_ranges_final:
+#     client.retrieve_mesh(x[0], x[1])
+#     print(f'{x[0]} -> {x[1]} {c}/{len(date_ranges_final)} done')
+#     c += 1
