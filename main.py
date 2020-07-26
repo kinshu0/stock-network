91abc5ebe742bac38f5b86631e5e2fb05f387b38
Kinshu Gupta <kinshugupta2002@gmail.com>
2020-07-26 17:55:12 -0400

    Made changes to make master the deployment branch instead of separate branches

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import json
+
 from core_modules.mongo_driver import mongo_connection as mongo_connection
 '''
 Establish connection with url and database name of mongoDB server
 '''
-mongo_connection('mongodb://localhost:27017/', 'dev3')
+with open('appconfig.json', 'r') as f:
+    configuration = json.load(f)
+
+mongo_connection(configuration['serverUrl'], configuration['dbName'])
 
 import webapp.front_end.web_application
 
